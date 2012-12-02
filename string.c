5161573f0a68ad58191129100d121095214494b7
Brian McKenzie <mckenzba@gmail.com>
2012-12-02 01:05:45 -0500

    implement atoi()

--- string.c
+++ string.c
@@ -32,3 +32,27 @@ unsigned int strlen(const char *str)
 
         return len;
 }
+
+int atoi(const char *str)
+{
+        int val = 0;
+        int neg = 0;
+
+        if (*str == '-') {
+            neg = 1;
+            *str++;
+        }
+
+        while (*str != '\0') {
+                if ( *str >= '0' || *str <= '9' ) {
+                    val *= 10;
+                    val += (int)(*str - '0');
+                }
+                *str++;
+        }
+
+        if (neg == 1)
+            val = 0 - val;
+
+        return val;
+}
--- string.h
+++ string.h
@@ -3,6 +3,7 @@
 #ifndef STRING_H
 #define STRING_H
 
+int atoi(const char *str);
 unsigned int strlen(const char *str);
 char *strcpy(char *restrict s1, const char *restrict s2);
 
